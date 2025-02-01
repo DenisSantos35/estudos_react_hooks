2b79b1a7bbc3dedc87cda7592bacb03a96367dfb
Denis Santos <denisdiogo18@gmail.com>
2025-02-01 09:17:18 -0300

    aninhando contextos e organizando por pastas

--- src/All_hooks/App/Join_of_hooks.jsx
+++ src/All_hooks/App/Join_of_hooks.jsx
@@ -1,14 +1,17 @@
 import { Posts } from '../../components/Posts';
+import { CounterProvider } from '../../contexts/Example_Provider';
 import { PostsProvider } from '../../contexts/Posts_Provider';
 import './App.css';
 
 function JoinOfHooks() {
   return (
-    <PostsProvider>
-      <div>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
 
--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -2,11 +2,19 @@ import { useContext, useEffect, useRef } from 'react';
 import './index.css';
 import { PostsContext } from '../../contexts/Posts_Provider/context';
 import { loadPosts } from '../../contexts/Posts_Provider/actions';
+import { CounterContext } from '../../contexts/Example_Provider/context';
+import {
+  incrementCounter,
+  decrementCounter,
+} from '../../contexts/Example_Provider/action';
 
 export const Posts = function () {
   const isMonted = useRef(true);
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     console.log(isMonted.current);
@@ -24,6 +32,12 @@ export const Posts = function () {
 
   return (
     <div className="App">
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter: {counterState.counter} +
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter: {counterState.counter} -
+      </button>
       <h1>POSTS</h1>
       {postsState.loading && (
         <p>
